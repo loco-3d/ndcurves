d0b7bd76b03a1e74260b2ee07a1fa8424486c961
stevet <stevetonneau@gotmail.fr>
2019-10-08 16:27:43 +0200

    fix z plot

--- python/test/sandbox/plot_bezier.py
+++ python/test/sandbox/plot_bezier.py
@@ -17,18 +17,16 @@ def plotControlPoints2D(bez, axes = [0,1], color = "r", ax = None):
     wps = [bez.waypointAtIndex(i) for i in range(bez.nbWaypoints)]
     x = np.array([wp[axes[0]] for wp in wps])
     y = np.array([wp[axes[1]] for wp in wps])
-    print "x ", x
-    print "y ", y
     if ax is not None:
         ax.scatter (x, y, color = color)
     else:        
         plt.scatter(x, y, color = color)
         
 def plotBezier(bez, step = 100., color = "b", ax = None):
-    points1 = np.array([(bez(i / step * bez.max())[axes[0]], bez(i / step * bez.max())[axes[1]]) for i in range(int(step))])
+    points1 = np.array([(bez(i / step * bez.max())[0], bez(i / step * bez.max())[1], bez(i / step * bez.max())[2]) for i in range(int(step))])
     x = points1[:, 0]
     y = points1[:, 1]
-    y = points1[:, 2]
+    z = points1[:, 2]
     if ax is not None:
         ax.plot (x, y, z, color, linewidth=2.0)
     else:        
@@ -64,6 +62,6 @@ if __name__ == '__main__':
     plotBezier2D(a, axes = [1,0], ax = ax)
     plotControlPoints2D(a, axes = [1,0], ax = ax)
     ax = fig.add_subplot(122, projection="3d")    
-    plotBezier2D(a, ax = ax)
-    plotControlPoints2D(a, ax = ax)
+    plotBezier(a, ax = ax)
+    plotControlPoints(a, ax = ax)
     plt.show(block=False)
