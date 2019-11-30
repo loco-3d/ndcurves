257c7ddcea8040a30fd0ecba47f4ba95878b909b
pfernbac <pierre.fernbach@gmail.com>
2019-11-30 19:52:51 +0100

    [Tests] update in change in compute_derivate : now return curve_ptr_t

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -145,17 +145,17 @@ void PolynomialCubicFunctionTest(bool& error) {
   }
   // Test derivate and compute_derivative
   // Order 1
-  polynomial_t cf_derivated = cf.compute_derivate(1);
-  ComparePoints(cf.derivate(0, 1), cf_derivated(0), errMsg + " - derivate order 1 : ", error);
-  ComparePoints(cf.derivate(0.3, 1), cf_derivated(0.3), errMsg + " - derivate order 1 : ", error);
-  ComparePoints(cf.derivate(0.5, 1), cf_derivated(0.5), errMsg + " - derivate order 1 : ", error);
-  ComparePoints(cf.derivate(1, 1), cf_derivated(1), errMsg + " - derivate order 1 : ", error);
+  curve_ptr_t cf_derivated = cf.compute_derivate(1);
+  ComparePoints(cf.derivate(0, 1), (*cf_derivated)(0), errMsg + " - derivate order 1 : ", error);
+  ComparePoints(cf.derivate(0.3, 1), (*cf_derivated)(0.3), errMsg + " - derivate order 1 : ", error);
+  ComparePoints(cf.derivate(0.5, 1), (*cf_derivated)(0.5), errMsg + " - derivate order 1 : ", error);
+  ComparePoints(cf.derivate(1, 1), (*cf_derivated)(1), errMsg + " - derivate order 1 : ", error);
   // Order 2
-  polynomial_t cf_derivated_2 = cf.compute_derivate(2);
-  ComparePoints(cf.derivate(0, 2), cf_derivated_2(0), errMsg + " - derivate order 1 : ", error);
-  ComparePoints(cf.derivate(0.3, 2), cf_derivated_2(0.3), errMsg + " - derivate order 1 : ", error);
-  ComparePoints(cf.derivate(0.5, 2), cf_derivated_2(0.5), errMsg + " - derivate order 1 : ", error);
-  ComparePoints(cf.derivate(1, 2), cf_derivated_2(1), errMsg + " - derivate order 1 : ", error);
+  curve_ptr_t cf_derivated_2 = cf.compute_derivate(2);
+  ComparePoints(cf.derivate(0, 2), (*cf_derivated_2)(0), errMsg + " - derivate order 1 : ", error);
+  ComparePoints(cf.derivate(0.3, 2), (*cf_derivated_2)(0.3), errMsg + " - derivate order 1 : ", error);
+  ComparePoints(cf.derivate(0.5, 2), (*cf_derivated_2)(0.5), errMsg + " - derivate order 1 : ", error);
+  ComparePoints(cf.derivate(1, 2), (*cf_derivated_2)(1), errMsg + " - derivate order 1 : ", error);
 }
 
 /*bezier_curve Function tests*/
@@ -197,11 +197,15 @@ void BezierCurveTest(bool& error) {
   // testing bernstein polynomials
   bezier_curve_t cf5(params.begin(), params.end(), 1., 2.);
   std::string errMsg2("In test BezierCurveTest ; Bernstein polynomials do not evaluate as analytical evaluation");
+  boost::shared_ptr<bezier_curve_t> cf5_derivated = boost::dynamic_pointer_cast<bezier_curve_t>(cf5.compute_derivate(1));
+
   for (double d = 1.; d < 2.; d += 0.1) {
     ComparePoints(cf5.evalBernstein(d), cf5(d), errMsg2, error);
     ComparePoints(cf5.evalHorner(d), cf5(d), errMsg2, error);
-    ComparePoints(cf5.compute_derivate(1).evalBernstein(d), cf5.compute_derivate(1)(d), errMsg2, error);
-    ComparePoints(cf5.compute_derivate(1).evalHorner(d), cf5.compute_derivate(1)(d), errMsg2, error);
+    ComparePoints(cf5_derivated->evalBernstein(d), cf5_derivated->operator()(d), errMsg2, error);
+    ComparePoints(cf5_derivated->evalHorner(d), cf5_derivated->operator()(d), errMsg2, error);
+    ComparePoints(cf5.derivate(d,1), cf5_derivated->operator()(d), errMsg2, error);
+
   }
   bool error_in(true);
   try {
@@ -327,7 +331,6 @@ void BezierDerivativeCurveTest(bool& error) {
   params.push_back(b);
   params.push_back(c);
   bezier_curve_t cf3(params.begin(), params.end());
-  ComparePoints(cf3(0), cf3.derivate(0., 0), errMsg, error);
   ComparePoints(cf3(0), cf3.derivate(0., 1), errMsg, error, true);
   ComparePoints(point3_t::Zero(), cf3.derivate(0., 100), errMsg, error);
 }
@@ -480,9 +483,9 @@ void cubicConversionTest(bool& error) {
   CompareCurves<bezier_curve_t, cubic_hermite_spline_t>(bc0, chs2, errMsg1, error);
 
   // Test : compute derivative of bezier => Convert it to polynomial
-  bezier_curve_t bc_der = bc0.compute_derivate(1);
-  polynomial_t pol_test = polynomial_from_curve<polynomial_t>(bc_der);
-  CompareCurves<bezier_curve_t, polynomial_t>(bc_der, pol_test, errMsg1, error);
+  curve_ptr_t bc_der = bc0.compute_derivate(1);
+  polynomial_t pol_test = polynomial_from_curve<polynomial_t>(*bc_der);
+  CompareCurves<curve_abc_t, polynomial_t>(*bc_der, pol_test, errMsg1, error);
 }
 
 /*Exact Cubic Function tests*/
@@ -1191,36 +1194,36 @@ void piecewiseCurveTest(bool& error) {
 
     // compare compute_derivate and derivate results :
 
-//    piecewise_curve_t pc_C2_derivate = pc_C2.compute_derivate(1);
-//    piecewise_curve_t pc_C2_derivate2 = pc_C2.compute_derivate(2);
-//    if (pc_C2.min() != pc_C2_derivate.min()) {
-//      error = true;
-//      std::cout << "min bounds for curve and it's derivate are not equals." << std::endl;
-//    }
-//    if (pc_C2.min() != pc_C2_derivate2.min()) {
-//      error = true;
-//      std::cout << "min bounds for curve and it's second derivate are not equals." << std::endl;
-//    }
-//    if (pc_C2.max() != pc_C2_derivate.max()) {
-//      error = true;
-//      std::cout << "max bounds for curve and it's derivate are not equals." << std::endl;
-//    }
-//    if (pc_C2.max() != pc_C2_derivate2.max()) {
-//      error = true;
-//      std::cout << "max bounds for curve and it's second derivate are not equals." << std::endl;
-//    }
-//    double t = 0.;
-//    while (t < pc_C2.max()) {
-//      if (!QuasiEqual(pc_C2.derivate(t, 1), pc_C2_derivate(t))) {
-//        error = true;
-//        std::cout << "value not equal between derivate and compute_derivate (order 1) at t = " << t << std::endl;
-//      }
-//      if (!QuasiEqual(pc_C2.derivate(t, 2), pc_C2_derivate2(t))) {
-//        error = true;
-//        std::cout << "value not equal between derivate and compute_derivate (order 2) at t = " << t << std::endl;
-//      }
-//      t += 0.01;
-//    }
+    curve_ptr_t pc_C2_derivate = pc_C2.compute_derivate(1);
+    curve_ptr_t pc_C2_derivate2 = pc_C2.compute_derivate(2);
+    if (pc_C2.min() != pc_C2_derivate->min()) {
+      error = true;
+      std::cout << "min bounds for curve and it's derivate are not equals." << std::endl;
+    }
+    if (pc_C2.min() != pc_C2_derivate2->min()) {
+      error = true;
+      std::cout << "min bounds for curve and it's second derivate are not equals." << std::endl;
+    }
+    if (pc_C2.max() != pc_C2_derivate->max()) {
+      error = true;
+      std::cout << "max bounds for curve and it's derivate are not equals." << std::endl;
+    }
+    if (pc_C2.max() != pc_C2_derivate2->max()) {
+      error = true;
+      std::cout << "max bounds for curve and it's second derivate are not equals." << std::endl;
+    }
+    double t = 0.;
+    while (t < pc_C2.max()) {
+      if (!QuasiEqual(pc_C2.derivate(t, 1), (*pc_C2_derivate)(t))) {
+        error = true;
+        std::cout << "value not equal between derivate and compute_derivate (order 1) at t = " << t << std::endl;
+      }
+      if (!QuasiEqual(pc_C2.derivate(t, 2), (*pc_C2_derivate2)(t))) {
+        error = true;
+        std::cout << "value not equal between derivate and compute_derivate (order 2) at t = " << t << std::endl;
+      }
+      t += 0.01;
+    }
 
   } catch (...) {
     error = true;
