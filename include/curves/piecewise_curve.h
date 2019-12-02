08ed37e9d432ccfe38936744984c9f751829d573
Pierre Fernbach <pierre.fernbach@laas.fr>
2019-12-02 14:42:47 +0100

    [piecewise] fix curve_at_time and curve_at_index API

--- include/curves/piecewise_curve.h
+++ include/curves/piecewise_curve.h
@@ -183,9 +183,9 @@ struct piecewise_curve : public curve_abc<Time, Numeric, Safe, Point,Point_deriv
 
   std::size_t num_curves() const { return curves_.size(); }
 
-  const curve_t& curve_at_time(const time_t t) const { return curves_[find_interval(t)]; }
+  const curve_ptr_t& curve_at_time(const time_t t) const { return curves_[find_interval(t)]; }
 
-  const curve_t& curve_at_index(const std::size_t idx) const {
+  const curve_ptr_t& curve_at_index(const std::size_t idx) const {
     if (Safe && idx >= num_curves()) {
       throw std::length_error(
           "curve_at_index: requested index greater than number of curves in piecewise_curve instance");
