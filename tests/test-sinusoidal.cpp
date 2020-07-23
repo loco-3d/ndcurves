6531599568533bef81f3d8093b16201f6fde7b5d
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-07-24 00:31:54 +0200

    [Tests] workaround Boost 1.72 unittest issue
    With Boost 1.72.0, if the sinusoidal comparison and the sinusoidal
    serialization tests are in the same test file, the tests succeed, but
    are followed by a "free(): invalid pointer" exception, marking the test
    as failed.
    
    This makes no sense, but the workaround is trivial.

--- tests/test-sinusoidal.cpp
+++ tests/test-sinusoidal.cpp
@@ -238,28 +238,4 @@ BOOST_AUTO_TEST_CASE(comparison) {
   BOOST_CHECK(c1 != cn6);
 }
 
-BOOST_AUTO_TEST_CASE(serialization) {
-  std::string fileName("fileTest_sinusoidal");
-  pointX_t p0(3), amp(3);
-  p0 << -1, 0.5, 2.;
-  amp << 2, -0.8, -1;
-  double T = 1.5;
-  double phi = 0.;
-
-  sinusoidal_t c(p0, amp, T, phi, 0., 20.);
-
-  c.saveAsText<sinusoidal_t>(fileName + ".txt");
-  c.saveAsXML<sinusoidal_t>(fileName + ".xml", "sinusoidal");
-  c.saveAsBinary<sinusoidal_t>(fileName);
-
-  sinusoidal_t c_txt, c_xml, c_binary;
-  c_txt.loadFromText<sinusoidal_t>(fileName + ".txt");
-  c_xml.loadFromXML<sinusoidal_t>(fileName + ".xml", "sinusoidal");
-  c_binary.loadFromBinary<sinusoidal_t>(fileName);
-
-  BOOST_CHECK(c == c_txt);
-  BOOST_CHECK(c == c_xml);
-  BOOST_CHECK(c == c_binary);
-}
-
 BOOST_AUTO_TEST_SUITE_END()
