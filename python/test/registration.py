906e28c6bbae602474778823dc2fd934dfd2c479
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-01-28 12:33:32 +0100

    Format: yapf 0.32

--- python/test/registration.py
+++ python/test/registration.py
@@ -7,6 +7,7 @@ class TestRegistration(unittest.TestCase):
     ref https://github.com/stack-of-tasks/eigenpy/issues/83
     ref https://gitlab.laas.fr/loco-3d/curves/-/issues/6
     """
+
     def test_pinocchio_then_curves(self):
         import pinocchio
         import ndcurves
