fa81ae8f245136c901af00d3d9dd2920bdba44d6
Carlos Mastalli <carlos.mastalli@gmail.com>
2023-07-30 08:32:54 +0100

    [copy] Improved copyable in curves

--- python/ndcurves/curves_python.cpp
+++ python/ndcurves/curves_python.cpp
@@ -1,6 +1,7 @@
 #ifdef CURVES_WITH_PINOCCHIO_SUPPORT
 #include <pinocchio/spatial/motion.hpp>
 #include <pinocchio/spatial/se3.hpp>
+#include "pinocchio/bindings/python/utils/copyable.hpp"
 #endif  // CURVES_WITH_PINOCCHIO_SUPPORT
 
 #include <boost/python.hpp>
@@ -166,37 +167,20 @@ struct curve_pickle_suite : pickle_suite {
   }
 };
 
-template <class T>
-inline PyObject* managingPyObject(T* p) {
-  return typename bp::manage_new_object::apply<T*>::type()(p);
-}
-
-template <class Copyable>
-bp::object generic__copy__(bp::object copyable) {
-  Copyable* newCopyable(new Copyable(bp::extract<const Copyable&>(copyable)));
-  bp::object result(bp::detail::new_reference(managingPyObject(newCopyable)));
-
-  bp::extract<bp::dict>(result.attr("__dict__"))().update(
-      copyable.attr("__dict__"));
-
-  return result;
-}
-
-template <class Copyable>
-bp::object generic__deepcopy__(bp::object copyable, bp::dict memo) {
-  bp::object copyMod = bp::import("copy");
-  bp::object deepcopy = copyMod.attr("deepcopy");
-
-  Copyable* newCopyable(new Copyable(bp::extract<const Copyable&>(copyable)));
-  bp::object result(bp::detail::new_reference(managingPyObject(newCopyable)));
-
-  int copyableId = (long long)(copyable.ptr());
-  memo[copyableId] = result;
-
-  bp::extract<bp::dict>(result.attr("__dict__"))().update(
-      deepcopy(bp::extract<bp::dict>(copyable.attr("__dict__"))(), memo));
-  return result;
-}
+/* Structure used to define copyable python curves */
+template<class C>
+struct copyable_suite : public bp::def_visitor< copyable_suite<C> > {
+  template<class PyClass>
+  void visit(PyClass & cl) const {
+     cl.def("copy",&copy,bp::arg("self"),"Returns a copy of *this.");
+     cl.def("__copy__", &copy,bp::arg("self"),"Returns a copy of *this.");
+     cl.def("__deepcopy__", &deepcopy,bp::args("self","memo"),"Returns a deep copy of *this.");
+  }
+      
+  private:
+     static C copy(const C & self) { return C(self); }
+     static C deepcopy(const C & self, bp::dict) { return C(self); }
+};
 
 template <class T>
 struct CopyableVisitor : public def_visitor<CopyableVisitor<T>> {
