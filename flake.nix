9b6facae83fd9279bcca696ce3947d1cb93cd3b5
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-11-10 12:29:04 +0100

    flake: sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -12,56 +12,41 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          apps.default = {
-            type = "app";
-            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-          };
-          packages = {
-            default = self'.packages.py-ndcurves;
-            ndcurves = pkgs.ndcurves.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./doc
-                  ./include
-                  ./package.xml
-                  ./python
-                  ./tests
-                ];
-              };
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, self, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          { gepetto-pkgs.overlays = [ self.overlays.default ]; }
+        ];
+        flake.overlays.default = _final: prev: {
+          ndcurves = prev.ndcurves.overrideAttrs {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./CMakeLists.txt
+                ./doc
+                ./include
+                ./package.xml
+                ./python
+                ./tests
+              ];
             };
-            py-ndcurves = pkgs.python3Packages.toPythonModule (
-              self'.packages.ndcurves.overrideAttrs (super: {
-                pname = "py-${super.pname}";
-                cmakeFlags = super.cmakeFlags ++ [
-                  (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
-                  (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" true)
-                ];
-                nativeBuildInputs = super.nativeBuildInputs ++ [
-                  pkgs.python3Packages.python
-                ];
-                propagatedBuildInputs = [
-                  pkgs.python3Packages.pinocchio
-                  self'.packages.ndcurves
-                ] ++ super.propagatedBuildInputs;
-                nativeCheckInputs = [
-                  pkgs.python3Packages.pythonImportsCheckHook
-                ];
-              })
-            );
           };
         };
-    };
+        perSystem =
+          { pkgs, self', ... }:
+          {
+            apps.default = {
+              type = "app";
+              program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
+            };
+            packages = {
+              default = self'.packages.ndcurves;
+              ndcurves = pkgs.python3Packages.ndcurves.override { standalone = false; };
+            };
+          };
+      }
+    );
 }
