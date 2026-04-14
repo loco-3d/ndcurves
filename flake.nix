e22614958a58024ddccc27e7fcb2144941256e34
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-14 17:33:14 +0200

    nix: mkFlakoboros

--- flake.nix
+++ flake.nix
@@ -1,39 +1,27 @@
 {
   description = "Library for creating smooth cubic splines";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros = {
-              extraDevPyPackages = [ "ndcurves" ];
-              overrideAttrs.ndcurves = {
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./CMakeLists.txt
-                    ./doc
-                    ./include
-                    ./package.xml
-                    ./python
-                    ./tests
-                  ];
-                };
-              };
-            };
-          }
-        ];
+        extraDevPyPackages = [ "ndcurves" ];
+        overrideAttrs.ndcurves = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./include
+              ./package.xml
+              ./python
+              ./tests
+            ];
+          };
+        };
       }
     );
 }
